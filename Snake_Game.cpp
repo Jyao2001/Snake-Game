e158c7c1b211e948c3fdd013f1571c780fb13abe
Jyao2001 <johnyao2001@gmail.com>
2020-12-04 14:06:33 -0600

    Update Snake_Game.cpp

--- Snake_Game.cpp
+++ Snake_Game.cpp
@@ -1,38 +1,3 @@
-/*
-    Snake_Game.cpp
-    1/22/2020 v5
-    John Yao
-    Create a perfected snake game
-    1337.003
-    changelog - 1/15/2020: added code to say why the game quits, fixed the # generator code
-                1/22/2020: separated some functions into sub-functions
-                1/25/2020: Added play again option
-
-    notes - things the reader of your code (including you, yourself) should be aware of (e.g., a list of things you might want to fix, change, add, etc. in future versions of the program)
-    comments - anything not included in the above that the reader should be aware of (e.g. external links to related information)
-    Wikipedia article on refactoring: https://en.wikipedia.org/wiki/Code_refactoring
-    //don't forget the random number generator
-
-    //////////////////////////////////////////////////////Guideline Check////////////////////////////////////////////////////////////////////////////////
-✓   1. Is there a bug in the board drawing code that sometimes makes the snake disappear? If there is, find it and fix it.
-    There may be other bugs in either the Logic or Draw routines; find any and fix them
-
-✓   2. Add the capability of having more than one fruit on the board.
-    Create some variable and set it to a value of 2 to 5, say, to represent the number of fruits that should be on the board.
-    You can either hardwire the number of fruits or ask the user for a number at the beginning of the program. Create a new fruit for each fruit that is eaten.
-
-✓   3.Add an option to let the user play another game when he loses rather than just exiting the program when the user fails.
-
-✓   4.Add an input option P that pauses the game and waits for the user to hit any other key before proceeding.
-
-✓   5.When the program first starts, print out some instructions for using the game to the user.
-
-✓   6. Add an option to the game that makes the player lose the game if the snake hits a wall.
-
-✓   7. Make the input routine case-insensitive.
-
-    8. Add any other features you think are valuable/interesting, and note in the comments at the top of the program what you added and why.
-*/
 #include <iostream> //for cout and cin
 #include <conio.h>  //for _kbhit and _getch
 #include <windows.h>    //for sleep
